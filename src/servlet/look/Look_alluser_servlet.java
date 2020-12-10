a733c8ca124216d7413e44cee1ff3ccc4ba59eb6
123-bots <1657271806@qq.com>
2020-12-10 08:39:02 +0800

    luojia

---
+++ src/servlet/look/Look_alluser_servlet.java
@@ -0,0 +1,45 @@
+package servlet.look;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import db.look.DB_lookuser;
+import filter.util.Filter_utils;
+import model.Admin;
+
+/**
+ * Servlet implementation class lookuser
+ */
+
+@WebServlet(name = "look_user_servlet", urlPatterns = {"/look_alluser_servlet" })
+public class Look_alluser_servlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HashMap<String, Object> hashMap = new HashMap<>();
+		Admin admin = new Admin();
+		ArrayList<Admin> b = DB_lookuser.alluser_look();
+		
+		if (b != null) {
+			hashMap.put("code", "200");
+			req.getSession().setAttribute("verify", Filter_utils.getIpAddr(req));
+		} else {
+			hashMap.put("code", "-1");
+		}
+		hashMap.put("data", b);
+		hashMap.put("length", b.size());
+		
+		resp.setContentType("application/json");
+		PrintWriter printWriter = resp.getWriter();
+		printWriter.write(Filter_utils.getJsonString(hashMap));
+		printWriter.flush();
+		printWriter.close();
+	}
+}
