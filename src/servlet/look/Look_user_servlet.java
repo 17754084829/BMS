a415b89f8b600c2fa6d91719545020ec812f3063
123-bots <1657271806@qq.com>
2020-12-10 08:37:17 +0800

    luojia

--- src/servlet/look/Look_user_servlet.java
+++ src/servlet/look/Look_user_servlet.java
@@ -18,15 +18,22 @@ import model.Admin;
 /**
  * Servlet implementation class lookuser
  */
-@WebServlet(name = "look_user_servlet", urlPatterns = {"/look/look_user_servlet" })
+
+@WebServlet(name = "look_user_servlet", urlPatterns = {"/look_user_servlet" })
 public class Look_user_servlet extends HttpServlet {
-	@Override
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HashMap<String, Object> hashMap = new HashMap<>();
-		int m = Integer.parseInt(req.getParameter("id")==null?"0":req.getParameter("id"));
 		Admin admin = new Admin();
-		admin.setId(m);
-		ArrayList<Admin> b = DB_lookuser.user_look(m);
+		String nmaes=req.getParameter("name");
+		int ids=Integer.parseInt(req.getParameter("id"));
+		
+		/*
+		String addtime1 = req.getParameter("addtime");
+		Date addtime2 = new SimpleDateFormat("yyyy-MM-dd").parse(addtime1);
+		*/
+		
+		ArrayList<Admin> b = DB_lookuser.user_look(nmaes, ids);
 		if (b != null) {
 			hashMap.put("200", "查询结果为：");
 			req.getSession().setAttribute("verify", Filter_utils.getIpAddr(req));
@@ -35,6 +42,7 @@ public class Look_user_servlet extends HttpServlet {
 		}
 		hashMap.put("data", b);
 		hashMap.put("length", b.size());
+		
 		resp.setContentType("application/json");
 		PrintWriter printWriter = resp.getWriter();
 		printWriter.write(Filter_utils.getJsonString(hashMap));
